c8c835f5598049080ed46b18429c7e56c844cc45
ayed87 <gake12234@gmail.com>
2025-02-22 02:58:50 +0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello, User!</h1>');
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server running at http://<public-ip>:${port}/`);
+});
